84834f8ef184b6a2a97c4914b07be3a95610cc99
Decors <decors1215@gmail.com>
2016-03-25 17:50:58 +0900

    Add message

--- init.fish
+++ init.fish
@@ -1,4 +1,10 @@
+if command ls --version 1>/dev/null 2>/dev/null
+    echo "lscolors plugin is not support GNU `ls`."
+    exit 1
+end
+
 if not type "lscolors" ^/dev/null >&2
     source functions/lscolors.fish
 end
+
 lscolors --silent
